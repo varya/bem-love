58b1e040ebcc84e846e78e74b42c97988a9980c7
Varvara Stepanova <toivonen@toivonen.ru>
2013-03-14 17:14:58 +0400

    container -> ask-her-what-music-she-likes

--- desktop.bundles/ask-her-what-music-she-likes/ask-her-what-music-she-likes.bemjson.js
+++ desktop.bundles/ask-her-what-music-she-likes/ask-her-what-music-she-likes.bemjson.js
@@ -2,10 +2,10 @@
     block: 'b-page',
     title: 'A boy, a girl and the heartless world',
     head: [
-        { elem: 'css', url: '_container.css', ie: false},
-        { elem: 'css', url: '_container', ie: true },
+        { elem: 'css', url: '_ask-her-what-music-she-likes.css', ie: false},
+        { elem: 'css', url: '_ask-her-what-music-she-likes', ie: true },
         { block: 'i-jquery', elem: 'core'},
-        { elem: 'js', url:'_container.js'},
+        { elem: 'js', url:'_ask-her-what-music-she-likes.js'},
     ],
     content: {
         block: 'world',
